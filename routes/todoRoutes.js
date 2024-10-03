40969429a8c0a7990804c772c8125ade8c8a442b
Ashish <dhakalashish18@gmail.com>
2024-10-03 22:33:16 +0545

    feat: get all todos api

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const todoController = require('../controllers/todoController');
 
 router.post('create-todo', todoController.CreateTodo);
+router.get('todos', todoController.GetAllTodos);
 
 module.exports = router;
