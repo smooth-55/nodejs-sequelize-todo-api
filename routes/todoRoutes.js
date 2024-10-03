ebf1135824c45fdf0ba8e85bc6b061a339e66c39
Ashish <dhakalashish18@gmail.com>
2024-10-03 22:36:20 +0545

    feat: update one todo by id api endpoint

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -5,5 +5,6 @@ const todoController = require('../controllers/todoController');
 router.post('create-todo', todoController.CreateTodo);
 router.get('todos', todoController.GetAllTodos);
 router.get('todo/:id', todoController.GetOneTodo);
+router.put('todo/:id', todoController.UpdateOneTodoById);
 
 module.exports = router;
