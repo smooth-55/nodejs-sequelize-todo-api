3dc7c0cf889256c84530743d8cadf7099f069598
Ashish <dhakalashish18@gmail.com>
2024-10-03 22:30:18 +0545

    feat: CreateTodo controllers

---
+++ controllers/todoController.js
@@ -0,0 +1,25 @@
+const Todo = require('../models/todoModel');
+const { response } = require('../response/response');
+const { TodoSchema } = require('../validators/todoValidator');
+
+
+// Create a New Todo
+const addTodo = async (req, res) => {
+    try {
+        const { error, value } = TodoSchema.validate(req.body);
+        if (error) {
+            response.BadReqest(res, error.details[0].message)
+        }
+        const newTodo = await Todo.create({
+            title: value?.title,
+            description: value?.description,
+            status: value?.status
+        });
+        response.Created(res, newTodo, "Todo created successfully")
+    } catch (err) {
+        response.InternalServerError(res, err.message)
+    }
+};
+
+
+module.exports = { addTodo };
