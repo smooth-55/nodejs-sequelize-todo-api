68b0b3162c14acee4224d809678024274480416b
Ashish <dhakalashish18@gmail.com>
2024-10-03 22:37:31 +0545

    chore: update api

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const todoController = require('../controllers/todoController');
 
-router.post('create-todo', todoController.CreateTodo);
+router.post('todo', todoController.CreateTodo);
 router.get('todos', todoController.GetAllTodos);
 router.get('todo/:id', todoController.GetOneTodo);
 router.put('todo/:id', todoController.UpdateOneTodoById);
