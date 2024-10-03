7115340fcaff5a56115c6181b6e74c0fb5603541
Ashish <dhakalashish18@gmail.com>
2024-10-03 22:35:10 +0545

    feat: get one todo by id api endpoint

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,5 +4,6 @@ const todoController = require('../controllers/todoController');
 
 router.post('create-todo', todoController.CreateTodo);
 router.get('todos', todoController.GetAllTodos);
+router.get('todo/:id', todoController.GetOneTodo);
 
 module.exports = router;
